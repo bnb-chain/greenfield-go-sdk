db550ca96fa4c7f4cd531c932ad1d7c0580b8fa7
Alexxxxxx <118710506+alexgao001@users.noreply.github.com>
2023-03-10 20:51:46 +0800

    fix: fix workflow and docs (#22)

--- client/test/config.go
+++ client/test/config.go
@@ -3,7 +3,7 @@ package test
 import "github.com/bnb-chain/greenfield-go-sdk/types"
 
 const (
-	TEST_PRIVATE_KEY = "2e876bb3980de05880dfb218179efb42291656d5ac59db71b6a475b9a41fb097"
+	TEST_PRIVATE_KEY = "d942508d5e4b2f0644d1ed736e5020d665ad706c0aed78a60fb4918ee9d60f15"
 	TEST_ADDR        = "0x76d244CE05c3De4BbC6fDd7F56379B145709ade9"
 	TEST_VAL_ADDR    = "0x80C7Fa8FC825C5e622cdbcAEa0A22d188634BDd3"
 	TEST_GRPC_ADDR   = "localhost:9090"
