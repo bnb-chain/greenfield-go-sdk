212be0e2e4db607d8a65fe214c4b656dca9bebfd
Alexgao001 <alex.g@nodereal.io>
2023-03-22 10:25:39 +0800

    remove qa env info

--- client/test/config.go
+++ client/test/config.go
@@ -3,18 +3,17 @@ package test
 import "github.com/bnb-chain/greenfield-go-sdk/types"
 
 const (
-	TEST_PRIVATE_KEY = "581c0cd6f99a2f0b00bb0d5775dc3835605303dbf180abb03d222808054221c4"
+	TEST_PRIVATE_KEY = "45cdca3d5687296eb2390aae1dce0d9f027dd993404185c6c2f43c3eaf72ae7f"
 	TEST_ADDR        = "0x76d244CE05c3De4BbC6fDd7F56379B145709ade9"
 	TEST_VAL_ADDR    = "0x80C7Fa8FC825C5e622cdbcAEa0A22d188634BDd3"
+	TEST_GRPC_ADDR   = "localhost:9090"
+	TEST_GRPC_ADDR2  = "localhost:9091"
+	TEST_GRPC_ADDR3  = "localhost:9092"
 
-	TEST_GRPC_ADDR  = "gnfd-grpc-plaintext.qa.bnbchain.world:9090"
-	TEST_GRPC_ADDR2 = "gnfd-grpc-plaintext.qa.bnbchain.world:9090"
-	TEST_GRPC_ADDR3 = "gnfd-grpc-plaintext.qa.bnbchain.world:9090"
+	TEST_RPC_ADDR  = "http://0.0.0.0:26750"
+	TEST_RPC_ADDR2 = "http://0.0.0.0:26751"
+	TEST_RPC_ADDR3 = "http://0.0.0.0:26752"
 
-	TEST_RPC_ADDR  = "https://gnfd.qa.bnbchain.world:443"
-	TEST_RPC_ADDR2 = "https://gnfd.qa.bnbchain.world:443"
-	TEST_RPC_ADDR3 = "https://gnfd.qa.bnbchain.world:443"
-
-	TEST_CHAIN_ID = "greenfield_9000-1741"
+	TEST_CHAIN_ID = "greenfield_9000-121"
 	TEST_DENOM    = types.Denom
 )
