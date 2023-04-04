9a389b20cca69a4ce6585974f273214ad3dc4bc8
Fynn <zcheng1004@gmail.com>
2023-04-04 11:41:29 +0800

    add more e2e test

--- e2e/e2e_basic_test.go
+++ e2e/e2e_basic_test.go
@@ -21,19 +21,6 @@ var (
 	ChainID     = "greenfield_9000-121"
 )
 
-func Test_Basic(t *testing.T) {
-	mnemonic := ParseValidatorMnemonic(0)
-	account, err := types.NewAccountFromMnemonic("test", mnemonic)
-	assert.NoError(t, err)
-	cli, err := client.New(ChainID, GrpcAddress, RpcAddress, &client.Option{DefaultAccount: account, GrpcDialOption: grpc.WithTransportCredentials(insecure.NewCredentials())})
-	assert.NoError(t, err)
-	ctx := context.Background()
-	status, err := cli.Status(ctx)
-	assert.NoError(t, err)
-
-	fmt.Println(status.SyncInfo.LatestBlockHeight)
-}
-
 // ParseValidatorMnemonic read the validator mnemonic from file
 func ParseValidatorMnemonic(i int) string {
 	return ParseMnemonicFromFile(fmt.Sprintf("../../greenfield/deployment/localup/.local/validator%d/info", i))
@@ -61,4 +48,24 @@ func ParseMnemonicFromFile(fileName string) string {
 		}
 	}
 	return line
+}
+
+func Test_Basic(t *testing.T) {
+	mnemonic := ParseValidatorMnemonic(0)
+	account, err := types.NewAccountFromMnemonic("test", mnemonic)
+	assert.NoError(t, err)
+	cli, err := client.New(ChainID, GrpcAddress, RpcAddress, &client.Option{DefaultAccount: account, GrpcDialOption: grpc.WithTransportCredentials(insecure.NewCredentials())})
+	assert.NoError(t, err)
+	ctx := context.Background()
+	status, err := cli.Status(ctx)
+	assert.NoError(t, err)
+
+	fmt.Println(status.SyncInfo.LatestBlockHeight)
+
+	heightBefore := status.SyncInfo.LatestBlockHeight
+	err = cli.WaitForBlockHeight(ctx, status.SyncInfo.LatestBlockHeight+10)
+	assert.NoError(t, err)
+	status, err = cli.Status(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, status.SyncInfo.LatestBlockHeight, heightBefore+10)
 }
