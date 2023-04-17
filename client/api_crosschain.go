bf6436afe901f0812d07e5267c0835423ef1d713
Alexgao001 <alex.g@nodereal.io>
2023-04-17 17:18:05 +0800

    add query sequence api

--- client/api_crosschain.go
+++ client/api_crosschain.go
@@ -7,6 +7,7 @@ import (
 	gnfdSdkTypes "github.com/bnb-chain/greenfield/sdk/types"
 	bridgetypes "github.com/bnb-chain/greenfield/x/bridge/types"
 	storagetypes "github.com/bnb-chain/greenfield/x/storage/types"
+	crosschaintypes "github.com/cosmos/cosmos-sdk/x/crosschain/types"
 	oracletypes "github.com/cosmos/cosmos-sdk/x/oracle/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -15,7 +16,11 @@ import (
 type CrossChain interface {
 	// TransferOut makes a transfer from Greenfield to BSC
 	TransferOut(ctx context.Context, toAddress string, amount math.Int, txOption *gnfdSdkTypes.TxOption) (*sdk.TxResponse, error)
+
 	Claims(ctx context.Context, fromAddr string, srcShainId, destChainId uint32, sequence uint64, timestamp uint64, payload []byte, voteAddrSet []uint64, aggSignature []byte, txOption *gnfdSdkTypes.TxOption) (*sdk.TxResponse, error)
+	GetChannelSendSequence(ctx context.Context, channelId uint32) (uint64, error)
+	GetChannelReceiveSequence(ctx context.Context, channelId uint32) (uint64, error)
+
 	MirrorGroup(ctx context.Context, operatorAddr string, id sdkmath.Uint, txOption *gnfdSdkTypes.TxOption) (*sdk.TxResponse, error)
 	MirrorBucket(ctx context.Context, operatorAddr string, id sdkmath.Uint, txOption *gnfdSdkTypes.TxOption) (*sdk.TxResponse, error)
 	MirrorObject(ctx context.Context, operatorAddr string, id sdkmath.Uint, txOption *gnfdSdkTypes.TxOption) (*sdk.TxResponse, error)
@@ -51,6 +56,28 @@ func (c *client) Claims(ctx context.Context, fromAddr string, srcShainId, destCh
 		return nil, err
 	}
 	return txResp.TxResponse, nil
+}
+
+func (c *client) GetChannelSendSequence(ctx context.Context, channelId uint32) (uint64, error) {
+	resp, err := c.chainClient.CrosschainQueryClient.SendSequence(
+		ctx,
+		&crosschaintypes.QuerySendSequenceRequest{ChannelId: channelId},
+	)
+	if err != nil {
+		return 0, err
+	}
+	return resp.Sequence, nil
+}
+
+func (c *client) GetChannelReceiveSequence(ctx context.Context, channelId uint32) (uint64, error) {
+	resp, err := c.chainClient.CrosschainQueryClient.ReceiveSequence(
+		ctx,
+		&crosschaintypes.QueryReceiveSequenceRequest{ChannelId: channelId},
+	)
+	if err != nil {
+		return 0, err
+	}
+	return resp.Sequence, nil
 }
 
 func (c *client) MirrorGroup(ctx context.Context, operatorAddr string, id sdkmath.Uint, txOption *gnfdSdkTypes.TxOption) (*sdk.TxResponse, error) {
