e08c7e19106b61beb37df3ce3ff1dfde40c7f122
Chris Li <271678682li@gmail.com>
2023-07-12 20:23:55 +0800

    add test for conflict sp

--- e2e/e2e_sp_exit_test.go
+++ e2e/e2e_sp_exit_test.go
@@ -49,9 +49,148 @@ func TestBucketMigrateTestSuiteTestSuite(t *testing.T) {
 	suite.Run(t, new(BucketMigrateTestSuite))
 }
 
-func (s *BucketMigrateTestSuite) Test_Bucket_Migrate_Object() {
+func (s *BucketMigrateTestSuite) CreateObjects(bucketName string, count int) ([]*types.ObjectDetail, []bytes.Buffer, error) {
+	var (
+		objectNames   []string
+		contentBuffer []bytes.Buffer
+		objectDetails []*types.ObjectDetail
+	)
+
+	// create object
+	for i := 0; i < count; i++ {
+		var buffer bytes.Buffer
+		line := `1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,123456789012`
+		// Create 1MiB content where each line contains 1024 characters.
+		for i := 0; i < 1024*3; i++ {
+			buffer.WriteString(fmt.Sprintf("[%05d] %s\n", i, line))
+		}
+		objectName := storageTestUtil.GenRandomObjectName()
+		s.T().Log("---> CreateObject and HeadObject <---")
+		objectTx, err := s.Client.CreateObject(s.ClientContext, bucketName, objectName, bytes.NewReader(buffer.Bytes()), types.CreateObjectOptions{})
+		s.Require().NoError(err)
+		_, err = s.Client.WaitForTx(s.ClientContext, objectTx)
+		s.Require().NoError(err)
+
+		objectNames = append(objectNames, objectName)
+		contentBuffer = append(contentBuffer, buffer)
+	}
+
+	// head object
+	time.Sleep(5 * time.Second)
+	for _, objectName := range objectNames {
+		objectDetail, err := s.Client.HeadObject(s.ClientContext, bucketName, objectName)
+		s.Require().NoError(err)
+		s.Require().Equal(objectDetail.ObjectInfo.ObjectName, objectName)
+		s.Require().Equal(objectDetail.ObjectInfo.GetObjectStatus().String(), "OBJECT_STATUS_CREATED")
+	}
+
+	s.T().Log("---> PutObject and GetObject <---")
+	for idx, objectName := range objectNames {
+		buffer := contentBuffer[idx]
+		err := s.Client.PutObject(s.ClientContext, bucketName, objectName, int64(buffer.Len()),
+			bytes.NewReader(buffer.Bytes()), types.PutObjectOptions{})
+		s.Require().NoError(err)
+	}
+
+	time.Sleep(20 * time.Second)
+	// seal object
+	for idx, objectName := range objectNames {
+		objectDetail, err := s.Client.HeadObject(s.ClientContext, bucketName, objectName)
+		s.Require().NoError(err)
+		s.Require().Equal(objectDetail.ObjectInfo.GetObjectStatus().String(), "OBJECT_STATUS_SEALED")
+
+		ior, info, err := s.Client.GetObject(s.ClientContext, bucketName, objectName, types.GetObjectOptions{})
+		s.Require().NoError(err)
+		if err == nil {
+			s.Require().Equal(info.ObjectName, objectName)
+			objectBytes, err := io.ReadAll(ior)
+			s.Require().NoError(err)
+			s.Require().Equal(objectBytes, contentBuffer[idx].Bytes())
+		}
+		objectDetails = append(objectDetails, objectDetail)
+	}
+
+	return objectDetails, contentBuffer, nil
+}
+
+//func (s *BucketMigrateTestSuite) Test_Bucket_Migrate_Object() {
+//	bucketName := storageTestUtil.GenRandomBucketName()
+//	objectName := storageTestUtil.GenRandomObjectName()
+//
+//	// 1) create bucket and object in srcSP
+//	bucketTx, err := s.Client.CreateBucket(s.ClientContext, bucketName, s.PrimarySP.OperatorAddress, types.CreateBucketOptions{})
+//	s.Require().NoError(err)
+//
+//	_, err = s.Client.WaitForTx(s.ClientContext, bucketTx)
+//	s.Require().NoError(err)
+//
+//	bucketInfo, err := s.Client.HeadBucket(s.ClientContext, bucketName)
+//	s.Require().NoError(err)
+//	if err == nil {
+//		s.Require().Equal(bucketInfo.Visibility, storageTypes.VISIBILITY_TYPE_PRIVATE)
+//	}
+//
+//	s.T().Log("---> CreateObject and HeadObject <---")
+//	s.CreateObjects(bucketName, 10)
+//
+//	// selete a storage provider to miragte
+//	sps, err := s.Client.ListStorageProviders(s.ClientContext, true)
+//	s.Require().NoError(err)
+//
+//	spIDs := make(map[uint32]bool)
+//	spIDs[objectDetail.GlobalVirtualGroup.PrimarySpId] = true
+//	for _, id := range objectDetail.GlobalVirtualGroup.SecondarySpIds {
+//		spIDs[id] = true
+//	}
+//	s.Require().Equal(len(spIDs), 7)
+//
+//	var destSP *types3.StorageProvider
+//	for _, sp := range sps {
+//		_, exist := spIDs[sp.Id]
+//		if !exist {
+//			destSP = &sp
+//			break
+//		}
+//	}
+//	s.Require().NotNil(destSP)
+//
+//	// send migrate bucket transaction
+//	txhash, err := s.Client.MigrateBucket(s.ClientContext, bucketName, types.MigrateBucketOptions{TxOpts: nil, DstPrimarySPID: 2, IsAsyncMode: false})
+//	s.Require().NoError(err)
+//
+//	tx, err := s.Client.WaitForTx(s.ClientContext, txhash)
+//	s.Require().NoError(err)
+//	s.T().Logf("VoteTx: %s", tx.TxHash)
+//
+//	bucketInfo, err = s.Client.HeadBucket(s.ClientContext, bucketName)
+//	s.Require().NoError(err)
+//	s.Require().Equal(bucketInfo.BucketStatus, storageTypes.BUCKET_STATUS_MIGRATING)
+//
+//	// TODO check migration whether success
+//	for {
+//		bucketInfo, err = s.Client.HeadBucket(s.ClientContext, bucketName)
+//		s.T().Logf("HeadBucket: %s", bucketInfo)
+//		s.Require().NoError(err)
+//		if bucketInfo.BucketStatus != storageTypes.BUCKET_STATUS_MIGRATING {
+//			break
+//		}
+//		time.Sleep(3 * time.Second)
+//	}
+//
+//	s.Require().Equal(bucketInfo.GetPrimarySpId(), destSP.GetId())
+//	ior, info, err := s.Client.GetObject(s.ClientContext, bucketName, objectName, types.GetObjectOptions{})
+//	s.Require().NoError(err)
+//	if err == nil {
+//		s.Require().Equal(info.ObjectName, objectName)
+//		objectBytes, err := io.ReadAll(ior)
+//		s.Require().NoError(err)
+//		s.Require().Equal(objectBytes, buffer.Bytes())
+//	}
+//}
+
+// test only one object's case
+func (s *BucketMigrateTestSuite) Test_Bucket_Migrate_Simple_Case() {
 	bucketName := storageTestUtil.GenRandomBucketName()
-	objectName := storageTestUtil.GenRandomObjectName()
 
 	// 1) create bucket and object in srcSP
 	bucketTx, err := s.Client.CreateBucket(s.ClientContext, bucketName, s.PrimarySP.OperatorAddress, types.CreateBucketOptions{})
@@ -66,43 +205,12 @@ func (s *BucketMigrateTestSuite) Test_Bucket_Migrate_Object() {
 		s.Require().Equal(bucketInfo.Visibility, storageTypes.VISIBILITY_TYPE_PRIVATE)
 	}
 
-	var buffer bytes.Buffer
-	line := `1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,123456789012`
-	// Create 1MiB content where each line contains 1024 characters.
-	for i := 0; i < 1024*30; i++ {
-		buffer.WriteString(fmt.Sprintf("[%05d] %s\n", i, line))
-	}
-
-	s.T().Log("---> CreateObject and HeadObject <---")
-	objectTx, err := s.Client.CreateObject(s.ClientContext, bucketName, objectName, bytes.NewReader(buffer.Bytes()), types.CreateObjectOptions{})
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, objectTx)
+	// test only one object's case
+	objectDetails, contentBuffer, err := s.CreateObjects(bucketName, 1)
 	s.Require().NoError(err)
 
-	time.Sleep(5 * time.Second)
-	objectDetail, err := s.Client.HeadObject(s.ClientContext, bucketName, objectName)
-	s.Require().NoError(err)
-	s.Require().Equal(objectDetail.ObjectInfo.ObjectName, objectName)
-	s.Require().Equal(objectDetail.ObjectInfo.GetObjectStatus().String(), "OBJECT_STATUS_CREATED")
-
-	s.T().Log("---> PutObject and GetObject <---")
-	err = s.Client.PutObject(s.ClientContext, bucketName, objectName, int64(buffer.Len()),
-		bytes.NewReader(buffer.Bytes()), types.PutObjectOptions{})
-	s.Require().NoError(err)
-
-	time.Sleep(20 * time.Second)
-	objectDetail, err = s.Client.HeadObject(s.ClientContext, bucketName, objectName)
-	s.Require().NoError(err)
-	s.Require().Equal(objectDetail.ObjectInfo.GetObjectStatus().String(), "OBJECT_STATUS_SEALED")
-
-	ior, info, err := s.Client.GetObject(s.ClientContext, bucketName, objectName, types.GetObjectOptions{})
-	s.Require().NoError(err)
-	if err == nil {
-		s.Require().Equal(info.ObjectName, objectName)
-		objectBytes, err := io.ReadAll(ior)
-		s.Require().NoError(err)
-		s.Require().Equal(objectBytes, buffer.Bytes())
-	}
+	objectDetail := objectDetails[0]
+	buffer := contentBuffer[0]
 
 	// selete a storage provider to miragte
 	sps, err := s.Client.ListStorageProviders(s.ClientContext, true)
@@ -125,24 +233,107 @@ func (s *BucketMigrateTestSuite) Test_Bucket_Migrate_Object() {
 	}
 	s.Require().NotNil(destSP)
 
-	// send migrate bucket transaction
+	s.T().Logf(":Migrate Bucket DstPrimarySPID %d", destSP.GetId())
+
+	// normal no conflict send migrate bucket transaction
 	txhash, err := s.Client.MigrateBucket(s.ClientContext, bucketName, types.MigrateBucketOptions{TxOpts: nil, DstPrimarySPID: destSP.GetId(), IsAsyncMode: false})
 	s.Require().NoError(err)
 
-	tx, err := s.Client.WaitForTx(s.ClientContext, txhash)
-	s.Require().NoError(err)
-	s.T().Logf("VoteTx: %s", tx.TxHash)
+	s.T().Logf("MigrateBucket : %s", txhash)
 
-	// TODO check migration whether success
-	//for {
-	//	p, err := s.Client.GetProposal(s.ClientContext, proposalID)
-	//	s.T().Logf("Proposal: %d, %s, %s, %s", p.Id, p.Status, p.VotingEndTime.String(), p.FinalTallyResult.String())
-	//	s.Require().NoError(err)
-	//	if p.Status == govTypesV1.ProposalStatus_PROPOSAL_STATUS_PASSED {
-	//		break
-	//	} else if p.Status == govTypesV1.ProposalStatus_PROPOSAL_STATUS_FAILED {
-	//		s.Require().True(false)
-	//	}
-	//	time.Sleep(1 * time.Second)
-	//}
+	for {
+		bucketInfo, err = s.Client.HeadBucket(s.ClientContext, bucketName)
+		s.T().Logf("HeadBucket: %s", bucketInfo)
+		s.Require().NoError(err)
+		if bucketInfo.BucketStatus != storageTypes.BUCKET_STATUS_MIGRATING {
+			break
+		}
+		time.Sleep(3 * time.Second)
+	}
+
+	s.Require().Equal(bucketInfo.GetPrimarySpId(), destSP.GetId())
+	ior, info, err := s.Client.GetObject(s.ClientContext, bucketName, objectDetail.ObjectInfo.ObjectName, types.GetObjectOptions{})
+	s.Require().NoError(err)
+	if err == nil {
+		s.Require().Equal(info.ObjectName, objectDetail.ObjectInfo.ObjectName)
+		objectBytes, err := io.ReadAll(ior)
+		s.Require().NoError(err)
+		s.Require().Equal(objectBytes, buffer.Bytes())
+	}
+}
+
+// test only conflict sp's case
+func (s *BucketMigrateTestSuite) Test_Bucket_Migrate_Simple_Conflict_Case() {
+	bucketName := storageTestUtil.GenRandomBucketName()
+
+	// 1) create bucket and object in srcSP
+	bucketTx, err := s.Client.CreateBucket(s.ClientContext, bucketName, s.PrimarySP.OperatorAddress, types.CreateBucketOptions{})
+	s.Require().NoError(err)
+
+	_, err = s.Client.WaitForTx(s.ClientContext, bucketTx)
+	s.Require().NoError(err)
+
+	bucketInfo, err := s.Client.HeadBucket(s.ClientContext, bucketName)
+	s.Require().NoError(err)
+	if err == nil {
+		s.Require().Equal(bucketInfo.Visibility, storageTypes.VISIBILITY_TYPE_PRIVATE)
+	}
+
+	// test only one object's case
+	objectDetails, contentBuffer, err := s.CreateObjects(bucketName, 1)
+	s.Require().NoError(err)
+
+	objectDetail := objectDetails[0]
+	buffer := contentBuffer[0]
+
+	// selete a storage provider to miragte
+	sps, err := s.Client.ListStorageProviders(s.ClientContext, true)
+	s.Require().NoError(err)
+
+	spIDs := make(map[uint32]bool)
+	spIDs[objectDetail.GlobalVirtualGroup.PrimarySpId] = true
+	for _, id := range objectDetail.GlobalVirtualGroup.SecondarySpIds {
+		spIDs[id] = true
+	}
+	s.Require().Equal(len(spIDs), 7)
+
+	var destSP *types3.StorageProvider
+	for _, sp := range sps {
+		_, exist := spIDs[sp.Id]
+		if !exist {
+			destSP = &sp
+			break
+		}
+	}
+	s.Require().NotNil(destSP)
+
+	s.T().Logf(":Migrate Bucket DstPrimarySPID %d", destSP.GetId())
+
+	// migrate bucket with conflict
+	conflictSPID := objectDetail.GlobalVirtualGroup.SecondarySpIds[0]
+	txhash, err := s.Client.MigrateBucket(s.ClientContext, bucketName, types.MigrateBucketOptions{TxOpts: nil, DstPrimarySPID: conflictSPID, IsAsyncMode: false})
+	s.Require().NoError(err)
+
+	s.T().Logf("MigrateBucket : %s", txhash)
+
+	for {
+		bucketInfo, err = s.Client.HeadBucket(s.ClientContext, bucketName)
+		s.T().Logf("HeadBucket: %s", bucketInfo)
+		s.Require().NoError(err)
+		if bucketInfo.BucketStatus != storageTypes.BUCKET_STATUS_MIGRATING {
+			break
+		}
+		time.Sleep(3 * time.Second)
+	}
+
+	s.Require().Equal(bucketInfo.GetPrimarySpId(), conflictSPID)
+	ior, info, err := s.Client.GetObject(s.ClientContext, bucketName, objectDetail.ObjectInfo.ObjectName, types.GetObjectOptions{})
+	s.Require().NoError(err)
+	if err == nil {
+		s.Require().Equal(info.ObjectName, objectDetail.ObjectInfo.ObjectName)
+		objectBytes, err := io.ReadAll(ior)
+		s.Require().NoError(err)
+		s.Require().Equal(objectBytes, buffer.Bytes())
+	}
+
 }
