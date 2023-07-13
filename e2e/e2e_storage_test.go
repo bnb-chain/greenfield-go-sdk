4bd8180676b8c5dcce2517b410d3d4a288bcb84e
Fynn <zcheng1004@gmail.com>
2023-07-13 21:19:10 +0800

    fix resumable download timeout

--- e2e/e2e_storage_test.go
+++ e2e/e2e_storage_test.go
@@ -393,7 +393,7 @@ func (s *StorageTestSuite) Test_Resumable_Upload_And_Download() {
 		bytes.NewReader(buffer.Bytes()), types.PutObjectOptions{PartSize: partSize})
 	s.Require().NoError(err)
 
-	time.Sleep(20 * time.Second)
+	time.Sleep(30 * time.Second)
 	objectDetail, err := s.Client.HeadObject(s.ClientContext, bucketName, objectName)
 	s.Require().NoError(err)
 	if err == nil {
