2000f9ecc859fd2c3538c98f878687e662243a53
flywukong <2229306838@qq.com>
2023-07-21 16:10:23 +0800

    fix e2e

--- e2e/e2e_storage_test.go
+++ e2e/e2e_storage_test.go
@@ -397,7 +397,7 @@ func (s *StorageTestSuite) Test_Resumable_Upload_And_Download() {
 		bytes.NewReader(buffer.Bytes()), types.PutObjectOptions{PartSize: partSize})
 	s.Require().NoError(err)
 
-	time.Sleep(140 * time.Second)
+	time.Sleep(150 * time.Second)
 	objectDetail, err := s.Client.HeadObject(s.ClientContext, bucketName, objectName)
 	s.Require().NoError(err)
 	if err == nil {
