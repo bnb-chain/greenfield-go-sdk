21277bcbf9184ddfd833dfcf3ab7847f79d40259
flywukong <2229306838@qq.com>
2023-08-14 13:35:49 +0800

    fix: fix err

--- e2e/e2e_storage_test.go
+++ e2e/e2e_storage_test.go
@@ -4,21 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"os"
-	"path/filepath"
-	"sort"
 	"sync"
 	"testing"
 	"time"
 
-	"cosmossdk.io/math"
-	"github.com/bnb-chain/greenfield-go-sdk/client"
 	"github.com/bnb-chain/greenfield-go-sdk/e2e/basesuite"
-	"github.com/bnb-chain/greenfield-go-sdk/pkg/utils"
 	"github.com/bnb-chain/greenfield-go-sdk/types"
-	types2 "github.com/bnb-chain/greenfield/sdk/types"
 	storageTestUtil "github.com/bnb-chain/greenfield/testutil/storage"
-	permTypes "github.com/bnb-chain/greenfield/x/permission/types"
 	spTypes "github.com/bnb-chain/greenfield/x/sp/types"
 	storageTypes "github.com/bnb-chain/greenfield/x/storage/types"
 	"github.com/stretchr/testify/suite"
@@ -44,93 +36,6 @@ func (s *StorageTestSuite) SetupSuite() {
 
 func TestStorageTestSuite(t *testing.T) {
 	suite.Run(t, new(StorageTestSuite))
-}
-
-func (s *StorageTestSuite) Test_Bucket() {
-	bucketName := storageTestUtil.GenRandomBucketName()
-
-	chargedQuota := uint64(100)
-	s.T().Log("---> CreateBucket and HeadBucket <---")
-	opts := types.CreateBucketOptions{ChargedQuota: chargedQuota}
-
-	bucketTx, err := s.Client.CreateBucket(s.ClientContext, bucketName, s.PrimarySP.OperatorAddress, opts)
-	s.Require().NoError(err)
-
-	_, err = s.Client.WaitForTx(s.ClientContext, bucketTx)
-	s.Require().NoError(err)
-
-	bucketInfo, err := s.Client.HeadBucket(s.ClientContext, bucketName)
-	s.Require().NoError(err)
-	if err == nil {
-		s.Require().Equal(bucketInfo.Visibility, storageTypes.VISIBILITY_TYPE_PRIVATE)
-		s.Require().Equal(bucketInfo.ChargedReadQuota, chargedQuota)
-	}
-
-	s.T().Log("--->  UpdateBucket <---")
-	updateBucketTx, err := s.Client.UpdateBucketVisibility(s.ClientContext, bucketName,
-		storageTypes.VISIBILITY_TYPE_PUBLIC_READ, types.UpdateVisibilityOption{})
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, updateBucketTx)
-	s.Require().NoError(err)
-
-	s.T().Log("---> BuyQuotaForBucket <---")
-	targetQuota := uint64(300)
-	buyQuotaTx, err := s.Client.BuyQuotaForBucket(s.ClientContext, bucketName, targetQuota, types.BuyQuotaOption{})
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, buyQuotaTx)
-	s.Require().NoError(err)
-
-	s.T().Log("---> Query Quota info <---")
-	quota, err := s.Client.GetBucketReadQuota(s.ClientContext, bucketName)
-	s.Require().NoError(err)
-	s.Require().Equal(quota.ReadQuotaSize, targetQuota)
-
-	s.T().Log("---> PutBucketPolicy <---")
-	principal, _, err := types.NewAccount("principal")
-	s.Require().NoError(err)
-
-	principalStr, err := utils.NewPrincipalWithAccount(principal.GetAddress())
-	s.Require().NoError(err)
-	statements := []*permTypes.Statement{
-		{
-			Effect: permTypes.EFFECT_ALLOW,
-			Actions: []permTypes.ActionType{
-				permTypes.ACTION_UPDATE_BUCKET_INFO,
-				permTypes.ACTION_DELETE_BUCKET,
-				permTypes.ACTION_CREATE_OBJECT,
-			},
-			Resources:      []string{},
-			ExpirationTime: nil,
-			LimitSize:      nil,
-		},
-	}
-	policy, err := s.Client.PutBucketPolicy(s.ClientContext, bucketName, principalStr, statements, types.PutPolicyOption{})
-	s.Require().NoError(err)
-
-	_, err = s.Client.WaitForTx(s.ClientContext, policy)
-	s.Require().NoError(err)
-
-	s.T().Log("---> GetBucketPolicy <---")
-	bucketPolicy, err := s.Client.GetBucketPolicy(s.ClientContext, bucketName, principal.GetAddress().String())
-	s.Require().NoError(err)
-	s.T().Logf("get bucket policy:%s\n", bucketPolicy.String())
-
-	s.T().Log("---> DeleteBucketPolicy <---")
-	deleteBucketPolicy, err := s.Client.DeleteBucketPolicy(s.ClientContext, bucketName, principalStr, types.DeletePolicyOption{})
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, deleteBucketPolicy)
-	s.Require().NoError(err)
-	_, err = s.Client.GetBucketPolicy(s.ClientContext, bucketName, principal.GetAddress().String())
-	s.Require().Error(err)
-
-	s.T().Log("--->  DeleteBucket <---")
-	delBucket, err := s.Client.DeleteBucket(s.ClientContext, bucketName, types.DeleteBucketOption{})
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, delBucket)
-	s.Require().NoError(err)
-
-	_, err = s.Client.HeadBucket(s.ClientContext, bucketName)
-	s.Require().Error(err)
 }
 
 func (s *StorageTestSuite) Test_Object() {
@@ -209,9 +114,6 @@ func (s *StorageTestSuite) Test_Object() {
 	fmt.Println("actual quota:", consumedQuota)
 	s.Require().Equal(uint64(expectQuotaUsed), consumedQuota)
 
-	quota0, err = s.Client.GetBucketReadQuota(s.ClientContext, bucketName)
-	s.Require().NoError(err)
-
 	var buffer2 bytes.Buffer
 	line2 := `1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,1234567890,123456789012`
 	// Create 1MiB content where each line contains 1024 characters.
@@ -242,11 +144,17 @@ func (s *StorageTestSuite) Test_Object() {
 	s.Require().NoError(err)
 	s.waitSealObject(bucketName, objectName2)
 
+	quota0, err = s.Client.GetBucketReadQuota(s.ClientContext, bucketName)
+	s.Require().NoError(err)
+
 	var wg sync.WaitGroup
 	wg.Add(concurrentNumber + 20)
+	downloadTime1 := 0
+	downloadTime2 := 0
 	for j := 0; j < concurrentNumber+20; j++ {
 		if j%2 == 0 {
 			go func() {
+				downloadTime1++
 				defer wg.Done()
 				for i := 0; i < 5; i++ {
 					objectContent, _, err := s.Client.GetObject(s.ClientContext, bucketName, objectName, types.GetObjectOptions{})
@@ -263,6 +171,7 @@ func (s *StorageTestSuite) Test_Object() {
 			}()
 		} else {
 			go func() {
+				downloadTime2++
 				defer wg.Done()
 				for i := 0; i < 5; i++ {
 					objectContent, _, err := s.Client.GetObject(s.ClientContext, bucketName, objectName2, types.GetObjectOptions{})
@@ -282,7 +191,8 @@ func (s *StorageTestSuite) Test_Object() {
 	wg.Wait()
 
 	concurrentOne := (concurrentNumber + 20 + 1) / 2
-	expectQuotaUsed = int(contentLen)*(concurrentOne)*downloadCount + int(contentLen2)*(concurrentOne)*downloadCount
+	fmt.Println("concurrentOne", concurrentOne, "download time,", downloadTime1, downloadTime2)
+	expectQuotaUsed = int(contentLen)*(downloadTime1)*downloadCount + int(contentLen2)*(downloadTime2)*downloadCount
 
 	fmt.Println("expect quota:", expectQuotaUsed)
 	quota1, err = s.Client.GetBucketReadQuota(s.ClientContext, bucketName)
@@ -293,94 +203,6 @@ func (s *StorageTestSuite) Test_Object() {
 
 	s.Require().Equal(uint64(expectQuotaUsed), consumedQuota)
 
-}
-
-func (s *StorageTestSuite) Test_Group() {
-	groupName := storageTestUtil.GenRandomGroupName()
-
-	groupOwner := s.DefaultAccount.GetAddress()
-	s.T().Log("---> CreateGroup and HeadGroup <---")
-	_, err := s.Client.CreateGroup(s.ClientContext, groupName, types.CreateGroupOptions{})
-	s.Require().NoError(err)
-	s.T().Logf("create GroupName: %s", groupName)
-
-	time.Sleep(5 * time.Second)
-	headResult, err := s.Client.HeadGroup(s.ClientContext, groupName, groupOwner.String())
-	s.Require().NoError(err)
-	s.Require().Equal(groupName, headResult.GroupName)
-
-	s.T().Log("---> Update GroupMember <---")
-	addAccount, _, err := types.NewAccount("member1")
-	s.Require().NoError(err)
-	updateMember := addAccount.GetAddress().String()
-	updateMembers := []string{updateMember}
-	expirationTimes := []time.Time{storageTypes.MaxTimeStamp}
-	txnHash, err := s.Client.UpdateGroupMember(s.ClientContext, groupName, groupOwner.String(), updateMembers, nil, expirationTimes, types.UpdateGroupMemberOption{})
-	s.T().Logf("add groupMember: %s", updateMembers[0])
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, txnHash)
-	s.Require().NoError(err)
-
-	// head added member
-	exist := s.Client.HeadGroupMember(s.ClientContext, groupName, groupOwner.String(), updateMember)
-	s.Require().Equal(true, exist)
-	if exist {
-		s.T().Logf("header groupMember: %s , exist", updateMembers[0])
-	}
-
-	// remove groupMember
-	txnHash, err = s.Client.UpdateGroupMember(s.ClientContext, groupName, groupOwner.String(), nil, updateMembers, nil, types.UpdateGroupMemberOption{})
-	s.T().Logf("remove groupMember: %s", updateMembers[0])
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, txnHash)
-	s.Require().NoError(err)
-
-	// head removed member
-	exist = s.Client.HeadGroupMember(s.ClientContext, groupName, groupOwner.String(), updateMember)
-	s.Require().Equal(false, exist)
-	if !exist {
-		s.T().Logf("header groupMember: %s , not exist", updateMembers[0])
-	}
-
-	s.T().Log("---> Set Group Permission<---")
-	grantUser, _, err := types.NewAccount("member2")
-	s.Require().NoError(err)
-
-	resp, err := s.Client.Transfer(s.ClientContext, grantUser.GetAddress().String(), math.NewIntWithDecimal(1, types2.DecimalBNB), types2.TxOption{})
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, resp)
-	s.Require().NoError(err)
-
-	statement := utils.NewStatement([]permTypes.ActionType{permTypes.ACTION_UPDATE_GROUP_MEMBER},
-		permTypes.EFFECT_ALLOW, nil, types.NewStatementOptions{})
-
-	// put group policy to another user
-	txnHash, err = s.Client.PutGroupPolicy(s.ClientContext, groupName, grantUser.GetAddress().String(),
-		[]*permTypes.Statement{&statement}, types.PutPolicyOption{})
-	s.Require().NoError(err)
-
-	s.T().Logf("put group policy to user %s", grantUser.GetAddress().String())
-	_, err = s.Client.WaitForTx(s.ClientContext, txnHash)
-	s.Require().NoError(err)
-	// use this user to update group
-	s.Client.SetDefaultAccount(grantUser)
-	s.Require().NoError(err)
-
-	// check permission, add back the member by grantClient
-	updateHash, err := s.Client.UpdateGroupMember(s.ClientContext, groupName, groupOwner.String(), updateMembers,
-		nil, expirationTimes, types.UpdateGroupMemberOption{})
-	s.Require().NoError(err)
-
-	_, err = s.Client.WaitForTx(s.ClientContext, updateHash)
-	s.Require().NoError(err)
-
-	s.Client.SetDefaultAccount(s.DefaultAccount)
-	// head removed member
-	exist = s.Client.HeadGroupMember(s.ClientContext, groupName, groupOwner.String(), updateMember)
-	s.Require().Equal(true, exist)
-	if exist {
-		s.T().Logf("header groupMember: %s , exist", updateMembers[0])
-	}
 }
 
 // UploadErrorHooker is a UploadPart hook---it will fail the 2nd segment's upload.
@@ -420,209 +242,4 @@ func (s *StorageTestSuite) waitSealObject(bucketName string, objectName string) 
 
 	s.Require().Equal(objectDetail.ObjectInfo.GetObjectStatus().String(), "OBJECT_STATUS_SEALED")
 	s.T().Logf("---> Wait Seal Object cost %d ms, <---", time.Since(startCheckTime).Milliseconds())
-}
-
-func (s *StorageTestSuite) createBigObjectWithoutPutObject() (bucket string, object string, objectbody bytes.Buffer) {
-	bucketName := storageTestUtil.GenRandomBucketName()
-	objectName := storageTestUtil.GenRandomObjectName()
-
-	bucketTx, err := s.Client.CreateBucket(s.ClientContext, bucketName, s.PrimarySP.OperatorAddress, types.CreateBucketOptions{})
-	s.Require().NoError(err)
-
-	_, err = s.Client.WaitForTx(s.ClientContext, bucketTx)
-	s.Require().NoError(err)
-
-	bucketInfo, err := s.Client.HeadBucket(s.ClientContext, bucketName)
-	s.Require().NoError(err)
-	if err == nil {
-		s.Require().Equal(bucketInfo.Visibility, storageTypes.VISIBILITY_TYPE_PRIVATE)
-	}
-
-	var buffer bytes.Buffer
-	// Create 45 MiB content, 3 segment
-	for i := 0; i < 1024*1500; i++ {
-		line := types.RandStr(20)
-		buffer.WriteString(fmt.Sprintf("[%05d] %s\n", i, line))
-	}
-
-	s.T().Log("---> CreateObject <---")
-	objectTx, err := s.Client.CreateObject(s.ClientContext, bucketName, objectName, bytes.NewReader(buffer.Bytes()), types.CreateObjectOptions{})
-	s.Require().NoError(err)
-	_, err = s.Client.WaitForTx(s.ClientContext, objectTx)
-	s.Require().NoError(err)
-
-	time.Sleep(5 * time.Second)
-	objectDetail, err := s.Client.HeadObject(s.ClientContext, bucketName, objectName)
-	s.Require().NoError(err)
-	s.Require().Equal(objectDetail.ObjectInfo.ObjectName, objectName)
-	s.Require().Equal(objectDetail.ObjectInfo.GetObjectStatus().String(), "OBJECT_STATUS_CREATED")
-
-	s.T().Logf("---> Create Bucket:%s, Object:%s <---", bucketName, objectName)
-
-	return bucketName, objectName, buffer
-}
-
-func getTmpFilesInDirectory(directory string) ([]string, error) {
-	var tmpFiles []string
-
-	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !info.IsDir() && filepath.Ext(path) == ".temp" {
-			tmpFiles = append(tmpFiles, path)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	sort.Slice(tmpFiles, func(i, j int) bool {
-		fileInfoI, _ := os.Stat(tmpFiles[i])
-		fileInfoJ, _ := os.Stat(tmpFiles[j])
-		return fileInfoI.ModTime().After(fileInfoJ.ModTime())
-	})
-
-	return tmpFiles, nil
-}
-
-func (s *StorageTestSuite) TruncateDownloadTempFileToLessPartsize() {
-	// Truncate file to less part size
-	dir, err := os.Getwd()
-	s.Require().NoError(err)
-	files, err := getTmpFilesInDirectory(dir)
-	s.Require().NoError(err)
-	tempFilePath := files[0]
-
-	file, err := os.OpenFile(tempFilePath, os.O_RDWR, 0o666)
-	s.Require().NoError(err)
-	defer file.Close()
-
-	fileInfo, err := file.Stat()
-	s.Require().NoError(err)
-	currentSize := fileInfo.Size()
-	targetSize := currentSize - 3*1024*1024
-
-	err = file.Truncate(targetSize)
-	s.T().Logf("---> Truncate file:%s to %d <---", tempFilePath, targetSize)
-	s.Require().NoError(err)
-}
-
-func (s *StorageTestSuite) Test_Resumable_Upload_And_Download() {
-	// 1) create big object without putobject
-	bucketName, objectName, buffer := s.createBigObjectWithoutPutObject()
-
-	s.T().Log("---> Resumable PutObject <---")
-	partSize16MB := uint64(1024 * 1024 * 16)
-	// 2) put a big object, the secondary segment will error, then resumable upload
-	client.UploadSegmentHooker = UploadErrorHooker
-	err := s.Client.PutObject(s.ClientContext, bucketName, objectName, int64(buffer.Len()),
-		bytes.NewReader(buffer.Bytes()), types.PutObjectOptions{PartSize: partSize16MB})
-	s.Require().ErrorContains(err, "UploadErrorHooker")
-	client.UploadSegmentHooker = client.DefaultUploadSegment
-	offset, err := s.Client.GetObjectResumableUploadOffset(s.ClientContext, bucketName, objectName)
-	s.Require().NoError(err)
-	s.Require().Equal(offset, partSize16MB)
-
-	err = s.Client.PutObject(s.ClientContext, bucketName, objectName, int64(buffer.Len()),
-		bytes.NewReader(buffer.Bytes()), types.PutObjectOptions{PartSize: partSize16MB})
-	s.Require().NoError(err)
-
-	s.waitSealObject(bucketName, objectName)
-
-	// 3) FGetObjectResumable compare with FGetObject
-	fileName := "test-file-" + storageTestUtil.GenRandomObjectName()
-	defer os.Remove(fileName)
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, fileName, types.GetObjectOptions{PartSize: 32 * 1024 * 1024})
-	s.T().Logf("--->  object file :%s <---", fileName)
-	s.Require().NoError(err)
-
-	fGetObjectFileName := "test-file-" + storageTestUtil.GenRandomObjectName()
-	defer os.Remove(fGetObjectFileName)
-	s.T().Logf("--->  object file :%s <---", fGetObjectFileName)
-	err = s.Client.FGetObject(s.ClientContext, bucketName, objectName, fGetObjectFileName, types.GetObjectOptions{})
-	s.Require().NoError(err)
-
-	isSame, err := types.CompareFiles(fileName, fGetObjectFileName)
-	s.Require().True(isSame)
-	s.Require().NoError(err)
-
-	// 4) Resumabledownload, download a file with default checkpoint
-	client.DownloadSegmentHooker = DownloadErrorHooker
-	resumableDownloadFile := storageTestUtil.GenRandomObjectName()
-	defer os.Remove(resumableDownloadFile)
-	s.T().Logf("---> Resumable download Create newfile:%s, <---", resumableDownloadFile)
-
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, resumableDownloadFile, types.GetObjectOptions{PartSize: 16 * 1024 * 1024})
-	s.Require().ErrorContains(err, "DownloadErrorHooker")
-	client.DownloadSegmentHooker = client.DefaultDownloadSegmentHook
-
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, resumableDownloadFile, types.GetObjectOptions{PartSize: 16 * 1024 * 1024})
-	s.Require().NoError(err)
-	// download success, checkpoint file has been deleted
-
-	isSame, err = types.CompareFiles(resumableDownloadFile, fGetObjectFileName)
-	s.Require().True(isSame)
-	s.Require().NoError(err)
-
-	// when the downloaded file size is less than a part size
-	client.DownloadSegmentHooker = DownloadErrorHooker
-	resumableDownloadLessPartFile := storageTestUtil.GenRandomObjectName()
-	defer os.Remove(resumableDownloadLessPartFile)
-	s.T().Logf("---> Resumable download for less part size , Create newfile:%s, <---", resumableDownloadLessPartFile)
-
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, resumableDownloadLessPartFile, types.GetObjectOptions{PartSize: 16 * 1024 * 1024})
-	s.Require().ErrorContains(err, "DownloadErrorHooker")
-
-	s.TruncateDownloadTempFileToLessPartsize()
-
-	client.DownloadSegmentHooker = client.DefaultDownloadSegmentHook
-
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, resumableDownloadLessPartFile, types.GetObjectOptions{PartSize: 16 * 1024 * 1024})
-	s.Require().NoError(err)
-	// download success, checkpoint file has been deleted
-
-	isSame, err = types.CompareFiles(resumableDownloadLessPartFile, fGetObjectFileName)
-	s.Require().True(isSame)
-	s.Require().NoError(err)
-
-	// 5) Resumabledownload, download a file with range
-	s.T().Logf("--->  Resumabledownload, download a file with range <---")
-	rangeOptions := types.GetObjectOptions{Range: "bytes=1000-94131999", PartSize: partSize16MB}
-	resumableDownloadWithRangeFile := "test-file-" + storageTestUtil.GenRandomObjectName()
-	defer os.Remove(resumableDownloadWithRangeFile)
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, resumableDownloadWithRangeFile, rangeOptions)
-	s.T().Logf("--->  object file :%s <---", resumableDownloadWithRangeFile)
-	s.Require().NoError(err)
-
-	fGetObjectWithRangeFile := "test-file-" + storageTestUtil.GenRandomObjectName()
-	defer os.Remove(fGetObjectWithRangeFile)
-	s.T().Logf("--->  object file :%s <---", fGetObjectWithRangeFile)
-	err = s.Client.FGetObject(s.ClientContext, bucketName, objectName, fGetObjectWithRangeFile, rangeOptions)
-	s.Require().NoError(err)
-
-	isSame, err = types.CompareFiles(resumableDownloadWithRangeFile, fGetObjectWithRangeFile)
-	s.Require().True(isSame)
-	s.Require().NoError(err)
-
-	// 6) Resumabledownload, download a file with range and Truncate
-	s.T().Logf("--->  Resumabledownload, download a file with range and Truncate <---")
-	rDownloadTruncateFile := "test-file-" + storageTestUtil.GenRandomObjectName()
-	defer os.Remove(rDownloadTruncateFile)
-	client.DownloadSegmentHooker = DownloadErrorHooker
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, rDownloadTruncateFile, rangeOptions)
-	s.T().Logf("--->  object file :%s <---", rDownloadTruncateFile)
-	s.Require().ErrorContains(err, "DownloadErrorHooker")
-	s.TruncateDownloadTempFileToLessPartsize()
-
-	client.DownloadSegmentHooker = client.DefaultDownloadSegmentHook
-	err = s.Client.FGetObjectResumable(s.ClientContext, bucketName, objectName, rDownloadTruncateFile, rangeOptions)
-	s.Require().NoError(err)
-
-	isSame, err = types.CompareFiles(rDownloadTruncateFile, fGetObjectWithRangeFile)
-	s.Require().True(isSame)
-	s.Require().NoError(err)
 }
