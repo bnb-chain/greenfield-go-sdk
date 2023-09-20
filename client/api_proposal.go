468b715da3bfb09703a7300a3860eca7c891f5de
yutianwu <wzxingbupt@gmail.com>
2023-09-20 15:29:28 +0800

    docs: refine the go doc of the cross-chain related apis

--- client/api_proposal.go
+++ client/api_proposal.go
@@ -19,6 +19,25 @@ type IProposalClient interface {
 	GetProposal(ctx context.Context, proposalID uint64) (*govTypesV1.Proposal, error)
 }
 
+// SubmitProposal - Submit a proposal to Greenfield.
+//
+// - ctx: Context variables for the current API call.
+//
+// - msgs: The messages to be executed when the proposal is passed.
+//
+// - depositAmount: The amount of BNB to deposit to the proposal.
+//
+// - title: The title of the proposal.
+//
+// - summary: The summary of the proposal.
+//
+// - opts: The options of the proposal.
+//
+// - ret1: Proposal id of the submitted proposal.
+//
+// - ret2: Transaction hash of the transaction.
+//
+// - ret3: Return error if the transaction failed, otherwise return nil.
 func (c *Client) SubmitProposal(ctx context.Context, msgs []sdk.Msg, depositAmount math.Int, title, summary string, opts types.SubmitProposalOptions) (uint64, string, error) {
 	msgSubmitProposal, err := govTypesV1.NewMsgSubmitProposal(msgs, sdk.NewCoins(sdk.NewCoin(gnfdSdkTypes.Denom, depositAmount)), c.defaultAccount.GetAddress().String(), opts.Metadata, title, summary)
 	if err != nil {
@@ -53,6 +72,19 @@ func (c *Client) SubmitProposal(ctx context.Context, msgs []sdk.Msg, depositAmou
 	return 0, txResp.TxResponse.TxHash, types.ErrorProposalIDNotFound
 }
 
+// VoteProposal - Vote for the proposal.
+//
+// - ctx: Context variables for the current API call.
+//
+// - proposalID: The proposal id to vote for.
+//
+// - voteOption: The vote option for the vote.
+//
+// - opts: The options of the transaction.
+//
+// - ret1: Transaction hash of the transaction.
+//
+// - ret2: Return error if the transaction failed, otherwise return nil.
 func (c *Client) VoteProposal(ctx context.Context, proposalID uint64, voteOption govTypesV1.VoteOption, opts types.VoteProposalOptions) (string, error) {
 	msgVote := govTypesV1.NewMsgVote(c.MustGetDefaultAccount().GetAddress(), proposalID, voteOption, opts.Metadata)
 	resp, err := c.chainClient.BroadcastTx(ctx, []sdk.Msg{msgVote}, &opts.TxOption)
@@ -62,6 +94,15 @@ func (c *Client) VoteProposal(ctx context.Context, proposalID uint64, voteOption
 	return resp.TxResponse.TxHash, nil
 }
 
+// GetProposal - Get the proposal by proposal id.
+//
+// - ctx: Context variables for the current API call.
+//
+// - proposalID: The proposal id to vote for.
+//
+// - ret1: Proposal by the queried proposal id.
+//
+// - ret2: Return error if the query failed, otherwise return nil.
 func (c *Client) GetProposal(ctx context.Context, proposalID uint64) (*govTypesV1.Proposal, error) {
 	resp, err := c.chainClient.GovQueryClientV1.Proposal(ctx, &govTypesV1.QueryProposalRequest{ProposalId: proposalID})
 	if err != nil {
