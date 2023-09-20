4b4622e3b0d144051678099b8286c8fc0d55c0e8
yutianwu <wzxingbupt@gmail.com>
2023-09-20 18:20:44 +0800

    docs: refine the go doc of the cross-chain related apis (#187)
    * docs: refine the go doc of the cross-chain related apis
    
    * fix comment

--- client/api_distribution.go
+++ client/api_distribution.go
@@ -16,7 +16,17 @@ type IDistributionClient interface {
 	FundCommunityPool(ctx context.Context, amount math.Int, txOption gnfdsdktypes.TxOption) (string, error)
 }
 
-// SetWithdrawAddress sets the withdrawal address for a delegator (or validator self-delegation).
+// SetWithdrawAddress - Set the withdrawal address for a delegator (or validator self-delegation).
+//
+// - ctx: Context variables for the current API call.
+//
+// - withdrawAddr: The withdrawal address to set.
+//
+// - txOption: The txOption for sending transactions.
+//
+// - ret1: Transaction hash of the transaction.
+//
+// - ret2: Return error if the transaction failed, otherwise return nil.
 func (c *Client) SetWithdrawAddress(ctx context.Context, withdrawAddr string, txOption gnfdsdktypes.TxOption) (string, error) {
 	withdraw, err := sdk.AccAddressFromHexUnsafe(withdrawAddr)
 	if err != nil {
@@ -30,7 +40,15 @@ func (c *Client) SetWithdrawAddress(ctx context.Context, withdrawAddr string, tx
 	return resp.TxResponse.TxHash, nil
 }
 
-// WithdrawValidatorCommission withdraw accumulated commission by validator
+// WithdrawValidatorCommission - Withdraw accumulated commission by validator.
+//
+// - ctx: Context variables for the current API call.
+//
+// - txOption: The txOption for sending transactions.
+//
+// - ret1: Transaction hash of the transaction.
+//
+// - ret2: Return error if the transaction failed, otherwise return nil.
 func (c *Client) WithdrawValidatorCommission(ctx context.Context, txOption gnfdsdktypes.TxOption) (string, error) {
 	msg := distrtypes.NewMsgWithdrawValidatorCommission(c.MustGetDefaultAccount().GetAddress())
 	resp, err := c.BroadcastTx(ctx, []sdk.Msg{msg}, &txOption)
@@ -40,7 +58,17 @@ func (c *Client) WithdrawValidatorCommission(ctx context.Context, txOption gnfds
 	return resp.TxResponse.TxHash, nil
 }
 
-// WithdrawDelegatorReward  withdraw rewards by a delegator
+// WithdrawDelegatorReward - Withdraw rewards by a delegator.
+//
+// - ctx: Context variables for the current API call.
+//
+// - validatorAddr: The validator address to withdraw from.
+//
+// - txOption: The txOption for sending transactions.
+//
+// - ret1: Transaction hash of the transaction.
+//
+// - ret2: Return error if the transaction failed, otherwise return nil.
 func (c *Client) WithdrawDelegatorReward(ctx context.Context, validatorAddr string, txOption gnfdsdktypes.TxOption) (string, error) {
 	validator, err := sdk.AccAddressFromHexUnsafe(validatorAddr)
 	if err != nil {
@@ -54,7 +82,17 @@ func (c *Client) WithdrawDelegatorReward(ctx context.Context, validatorAddr stri
 	return resp.TxResponse.TxHash, nil
 }
 
-// FundCommunityPool sends coins directly from the sender to the community pool.
+// FundCommunityPool - Sends coins directly from the sender to the community pool.
+//
+// - ctx: Context variables for the current API call.
+//
+// - amount: The amount of BNB to send.
+//
+// - txOption: The txOption for sending transactions.
+//
+// - ret1: Transaction hash of the transaction.
+//
+// - ret2: Return error if the transaction failed, otherwise return nil.
 func (c *Client) FundCommunityPool(ctx context.Context, amount math.Int, txOption gnfdsdktypes.TxOption) (string, error) {
 	msg := distrtypes.NewMsgFundCommunityPool(sdk.Coins{sdk.Coin{Denom: gnfdsdktypes.Denom, Amount: amount}}, c.MustGetDefaultAccount().GetAddress())
 	resp, err := c.BroadcastTx(ctx, []sdk.Msg{msg}, &txOption)
