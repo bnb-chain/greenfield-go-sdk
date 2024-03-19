dbd5171908b82356665a88869c085d9331b983df
constwz <122766871+constwz@users.noreply.github.com>
2024-03-19 17:19:32 +0800

    fix: get bucket quota (#232)

--- types/list.go
+++ types/list.go
@@ -8,14 +8,16 @@ import (
 
 // QuotaInfo indicates the quota info of bucket
 type QuotaInfo struct {
-	XMLName             xml.Name `xml:"GetReadQuotaResult"`
-	Version             string   `xml:"version,attr"`
-	BucketName          string   `xml:"BucketName"`
-	BucketID            string   `xml:"BucketID"`            // BucketID defines the bucket read quota value on chain
-	ReadQuotaSize       uint64   `xml:"ReadQuotaSize"`       // ReadQuotaSize defines the bucket read quota value on chain
-	SPFreeReadQuotaSize uint64   `xml:"SPFreeReadQuotaSize"` // SPFreeReadQuotaSize defines the free quota of this month
-	ReadConsumedSize    uint64   `xml:"ReadConsumedSize"`    // ReadConsumedSize defines the consumed total read quota of this month
-	FreeConsumedSize    uint64   `xml:"FreeConsumedSize"`    // FreeConsumedSize defines the consumed free quota
+	XMLName                 xml.Name `xml:"GetReadQuotaResult"`
+	Version                 string   `xml:"version,attr"`
+	BucketName              string   `xml:"BucketName"`
+	BucketID                string   `xml:"BucketID"`                 // BucketID defines the bucket read quota value on chain
+	ReadQuotaSize           uint64   `xml:"ReadQuotaSize"`            // ReadQuotaSize defines the bucket read quota value on chain
+	SPFreeReadQuotaSize     uint64   `xml:"SPFreeReadQuotaSize"`      // SPFreeReadQuotaSize defines the free quota of this month
+	ReadConsumedSize        uint64   `xml:"ReadConsumedSize"`         // ReadConsumedSize defines the consumed total read quota of this month
+	FreeConsumedSize        uint64   `xml:"FreeConsumedSize"`         // FreeConsumedSize defines the consumed free quota
+	MonthlyFreeQuota        uint64   `xml:"MonthlyFreeQuota"`         // MonthlyFreeQuota defines the consumed monthly free quota
+	MonthlyFreeConsumedSize uint64   `xml:"MonthlyQuotaConsumedSize"` // MonthlyFreeConsumedSize defines the consumed monthly free quota
 }
 
 // ReadRecord indicate the download record info
