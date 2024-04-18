c4eced7314634c55922526138ee3a8d07447cdcd
BarryTong65 <barrytong.work@gmail.com>
2024-04-18 16:13:05 +0800

    fix: format code

--- bscexamples/multi_messages.go
+++ bscexamples/multi_messages.go
@@ -3,14 +3,16 @@ package main
 import (
 	"context"
 	"log"
+	"math/big"
 	"time"
 
 	"cosmossdk.io/math"
-	"github.com/bnb-chain/greenfield-go-sdk/bsc"
-	"github.com/bnb-chain/greenfield-go-sdk/bsctypes"
 	"github.com/bnb-chain/greenfield/types/resource"
 	"github.com/bnb-chain/greenfield/x/permission/types"
 	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/bnb-chain/greenfield-go-sdk/bsc"
+	"github.com/bnb-chain/greenfield-go-sdk/bsctypes"
 )
 
 func main() {
@@ -34,6 +36,7 @@ func main() {
 		log.Fatalf("unable to get min ack relay fee, %v", err)
 	}
 
+	// fluent interface example, executing transactions in the order specified by the user.
 	messages := bsctypes.NewMessages(client.GetDeployment(), relayFee, minAckRelayFee, gasPrice)
 	primarySpAddress := common.HexToAddress("0xd142052d8c0881fc7485c1270c3510bc442e05dd")
 	_ = messages.CreateBucket(account.GetAddress(), &bsctypes.CreateBucketSynPackage{
@@ -57,139 +60,145 @@ func main() {
 			},
 		},
 	}).CreateGroup(account.GetAddress(), account.GetAddress(), "test-group-1")
-	//_ = messages.CreatePolicy(account.GetAddress(), &types.Policy{
-	//	Principal: &types.Principal{
-	//		Type:  types.PRINCIPAL_TYPE_GNFD_ACCOUNT,
-	//		Value: "0x0C02787e83948e7aD29abE3a99b29c480f9F0096",
-	//	},
-	//	ResourceType: resource.RESOURCE_TYPE_BUCKET,
-	//	ResourceId:   math.NewUint(179373),
-	//	Statements: []*types.Statement{
-	//		&types.Statement{
-	//			Effect:  types.EFFECT_ALLOW,
-	//			Actions: []types.ActionType{types.ACTION_DELETE_BUCKET},
-	//		},
-	//	},
-	//})
 
-	//FAIL BARRY
-	//_ = messages.CreatePolicyCallBack(
-	//	account.GetAddress(),
-	//	&types.Policy{
-	//		Principal: &types.Principal{
-	//			Type:  types.PRINCIPAL_TYPE_GNFD_ACCOUNT,
-	//			Value: "0x2b1A0Ba5a484ef9375DF0A908096f1da6d58d86a",
-	//		},
-	//		ResourceType: resource.RESOURCE_TYPE_BUCKET,
-	//		ResourceId:   math.NewUint(179373),
-	//		Statements: []*types.Statement{
-	//			&types.Statement{
-	//				Effect:  types.EFFECT_ALLOW,
-	//				Actions: []types.ActionType{types.ACTION_DELETE_BUCKET},
-	//			},
-	//		},
-	//	},
-	//	&bsctypes.ExtraData{
-	//		AppAddress:            account.GetAddress(),
-	//		RefundAddress:         account.GetAddress(),
-	//		FailureHandleStrategy: 0,
-	//	}, nil)
+	// transfer out money from bsc to greenfield
+	_ = messages.TransferOut(account.GetAddress(), account.GetAddress(), big.NewInt(1e16))
 
-	//_ = messages.DeletePolicy(account.GetAddress(), big.NewInt(442))
+	// Create policy
+	_ = messages.CreatePolicy(account.GetAddress(), &types.Policy{
+		Principal: &types.Principal{
+			Type:  types.PRINCIPAL_TYPE_GNFD_ACCOUNT,
+			Value: "0x0C02787e83948e7aD29abE3a99b29c480f9F0096",
+		},
+		ResourceType: resource.RESOURCE_TYPE_BUCKET,
+		ResourceId:   math.NewUint(179373),
+		Statements: []*types.Statement{
+			&types.Statement{
+				Effect:  types.EFFECT_ALLOW,
+				Actions: []types.ActionType{types.ACTION_DELETE_BUCKET},
+			},
+		},
+	})
 
-	// FAIL BARRY
-	//_ = messages.DeletePolicyCallBack(account.GetAddress(), big.NewInt(447),
-	//	&bsctypes.ExtraData{
-	//		AppAddress:            account.GetAddress(),
-	//		RefundAddress:         account.GetAddress(),
-	//		FailureHandleStrategy: 0,
-	//	}, nil)
+	_ = messages.CreatePolicyCallBack(
+		account.GetAddress(),
+		&types.Policy{
+			Principal: &types.Principal{
+				Type:  types.PRINCIPAL_TYPE_GNFD_ACCOUNT,
+				Value: "0x2b1A0Ba5a484ef9375DF0A908096f1da6d58d86a",
+			},
+			ResourceType: resource.RESOURCE_TYPE_BUCKET,
+			ResourceId:   math.NewUint(179373),
+			Statements: []*types.Statement{
+				&types.Statement{
+					Effect:  types.EFFECT_ALLOW,
+					Actions: []types.ActionType{types.ACTION_DELETE_BUCKET},
+				},
+			},
+		},
+		&bsctypes.ExtraData{
+			AppAddress:            account.GetAddress(),
+			RefundAddress:         account.GetAddress(),
+			FailureHandleStrategy: 0,
+		}, nil)
 
-	// success test cases
-	//_ = messages.TransferOut(account.GetAddress(), account.GetAddress(), big.NewInt(1e16))
+	//Delete Policy
+	_ = messages.DeletePolicy(account.GetAddress(), big.NewInt(442))
 
-	// _ = messages.CreateGroup(account.GetAddress(), account.GetAddress(), "test-policy")
-	//_ = messages.CreateGroupCallBack(account.GetAddress(), account.GetAddress(), "test-callback", big.NewInt(1000000), &bsctypes.ExtraData{
-	//	AppAddress:            account.GetAddress(),
-	//	RefundAddress:         account.GetAddress(),
-	//	FailureHandleStrategy: bsctypes.SkipOnFail,
-	//}, nil)
+	_ = messages.DeletePolicyCallBack(account.GetAddress(), big.NewInt(447),
+		&bsctypes.ExtraData{
+			AppAddress:            account.GetAddress(),
+			RefundAddress:         account.GetAddress(),
+			FailureHandleStrategy: 0,
+		}, nil)
 
-	//_ = messages.UpdateGroup(account.GetAddress(), &bsctypes.UpdateGroupMemberSynPackage{
-	//	Operator:         account.GetAddress(),
-	//	Id:               big.NewInt(398),
-	//	OpType:           bsctypes.AddMembers,
-	//	Members:          []common.Address{common.HexToAddress("0xcdffb8d585d954dfb7b00c76af4f173c6a8e3549")},
-	//	MemberExpiration: []uint64{1733412997},
-	//})
+	// Crete group
+	_ = messages.CreateGroup(account.GetAddress(), account.GetAddress(), "test-policy")
+	_ = messages.CreateGroupCallBack(account.GetAddress(), account.GetAddress(), "test-callback", big.NewInt(1000000), &bsctypes.ExtraData{
+		AppAddress:            account.GetAddress(),
+		RefundAddress:         account.GetAddress(),
+		FailureHandleStrategy: bsctypes.SkipOnFail,
+	}, nil)
 
-	//_ = messages.UpdateGroupCallBack(account.GetAddress(), &bsctypes.UpdateGroupMemberSynPackage{
-	//	Operator:         account.GetAddress(),
-	//	Id:               big.NewInt(398),
-	//	OpType:           bsctypes.AddMembers,
-	//	Members:          []common.Address{common.HexToAddress("0x5fa8b3f3fcd4a3ea2495e11dd5dbd399b3d8d4f8")},
-	//	MemberExpiration: []uint64{1733412997},
-	//}, big.NewInt(1000000),
-	//	&bsctypes.ExtraData{
-	//		AppAddress:            account.GetAddress(),
-	//		RefundAddress:         account.GetAddress(),
-	//		FailureHandleStrategy: 0,
-	//	}, nil)
+	// update group
+	_ = messages.UpdateGroup(account.GetAddress(), &bsctypes.UpdateGroupMemberSynPackage{
+		Operator:         account.GetAddress(),
+		Id:               big.NewInt(398),
+		OpType:           bsctypes.AddMembers,
+		Members:          []common.Address{common.HexToAddress("0xcdffb8d585d954dfb7b00c76af4f173c6a8e3549")},
+		MemberExpiration: []uint64{1733412997},
+	})
 
-	// 	_ = messages.DeleteGroup(account.GetAddress(), big.NewInt(398))
+	_ = messages.UpdateGroupCallBack(account.GetAddress(), &bsctypes.UpdateGroupMemberSynPackage{
+		Operator:         account.GetAddress(),
+		Id:               big.NewInt(398),
+		OpType:           bsctypes.AddMembers,
+		Members:          []common.Address{common.HexToAddress("0x5fa8b3f3fcd4a3ea2495e11dd5dbd399b3d8d4f8")},
+		MemberExpiration: []uint64{1733412997},
+	}, big.NewInt(1000000),
+		&bsctypes.ExtraData{
+			AppAddress:            account.GetAddress(),
+			RefundAddress:         account.GetAddress(),
+			FailureHandleStrategy: 0,
+		}, nil)
 
-	//_ = messages.DeleteGroupCallBack(account.GetAddress(), big.NewInt(404), big.NewInt(1000000),
-	//	&bsctypes.ExtraData{
-	//		AppAddress:            account.GetAddress(),
-	//		RefundAddress:         account.GetAddress(),
-	//		FailureHandleStrategy: 0,
-	//	}, nil)
+	// Delete group
+	_ = messages.DeleteGroup(account.GetAddress(), big.NewInt(398))
 
-	//primarySpAddress := common.HexToAddress("0xd142052d8c0881fc7485c1270c3510bc442e05dd")
-	//_ = messages.CreateBucket(account.GetAddress(), &bsctypes.CreateBucketSynPackage{
-	//	Creator:                    account.GetAddress(),
-	//	Name:                       "cross-chain-test-4",
-	//	Visibility:                 1,
-	//	PaymentAddress:             account.GetAddress(),
-	//	PrimarySpAddress:           &primarySpAddress,
-	//	GlobalVirtualGroupFamilyId: 1,
-	//})
+	_ = messages.DeleteGroupCallBack(account.GetAddress(), big.NewInt(404), big.NewInt(1000000),
+		&bsctypes.ExtraData{
+			AppAddress:            account.GetAddress(),
+			RefundAddress:         account.GetAddress(),
+			FailureHandleStrategy: 0,
+		}, nil)
 
-	//_ = messages.CreateBucketCallBack(
-	//	account.GetAddress(),
-	//	&bsctypes.CreateBucketSynPackage{
-	//		Creator:                    account.GetAddress(),
-	//		Name:                       "cross-chain-test-2",
-	//		Visibility:                 1,
-	//		PaymentAddress:             account.GetAddress(),
-	//		PrimarySpAddress:           &primarySpAddress,
-	//		GlobalVirtualGroupFamilyId: 1,
-	//	},
-	//	big.NewInt(1000000),
-	//	&bsctypes.ExtraData{
-	//		AppAddress:            account.GetAddress(),
-	//		RefundAddress:         account.GetAddress(),
-	//		FailureHandleStrategy: 0,
-	//	}, nil)
+	// create bucket
+	_ = messages.CreateBucket(account.GetAddress(), &bsctypes.CreateBucketSynPackage{
+		Creator:                    account.GetAddress(),
+		Name:                       "cross-chain-test-4",
+		Visibility:                 1,
+		PaymentAddress:             account.GetAddress(),
+		PrimarySpAddress:           &primarySpAddress,
+		GlobalVirtualGroupFamilyId: 1,
+	})
 
-	//_ = messages.DeleteBucket(account.GetAddress(), big.NewInt(179370))
-	//
-	//_ = messages.DeleteBucketCallBack(account.GetAddress(), big.NewInt(179372), big.NewInt(100000),
-	//	&bsctypes.ExtraData{
-	//		AppAddress:            account.GetAddress(),
-	//		RefundAddress:         account.GetAddress(),
-	//		FailureHandleStrategy: 0,
-	//	}, nil)
+	_ = messages.CreateBucketCallBack(
+		account.GetAddress(),
+		&bsctypes.CreateBucketSynPackage{
+			Creator:                    account.GetAddress(),
+			Name:                       "cross-chain-test-2",
+			Visibility:                 1,
+			PaymentAddress:             account.GetAddress(),
+			PrimarySpAddress:           &primarySpAddress,
+			GlobalVirtualGroupFamilyId: 1,
+		},
+		big.NewInt(1000000),
+		&bsctypes.ExtraData{
+			AppAddress:            account.GetAddress(),
+			RefundAddress:         account.GetAddress(),
+			FailureHandleStrategy: 0,
+		}, nil)
 
-	//_ = messages.DeleteObject(account.GetAddress(), big.NewInt(422460))
+	//delete bucket
+	_ = messages.DeleteBucket(account.GetAddress(), big.NewInt(179370))
 
-	//_ = messages.DeleteObjectCallBack(account.GetAddress(), big.NewInt(422445), big.NewInt(1000000),
-	//	&bsctypes.ExtraData{
-	//		AppAddress:            account.GetAddress(),
-	//		RefundAddress:         account.GetAddress(),
-	//		FailureHandleStrategy: 0,
-	//		CallbackData:          []byte{'1'},
-	//	}, nil)
+	_ = messages.DeleteBucketCallBack(account.GetAddress(), big.NewInt(179372), big.NewInt(100000),
+		&bsctypes.ExtraData{
+			AppAddress:            account.GetAddress(),
+			RefundAddress:         account.GetAddress(),
+			FailureHandleStrategy: 0,
+		}, nil)
+
+	// delele object
+	_ = messages.DeleteObject(account.GetAddress(), big.NewInt(422460))
+
+	_ = messages.DeleteObjectCallBack(account.GetAddress(), big.NewInt(422445), big.NewInt(1000000),
+		&bsctypes.ExtraData{
+			AppAddress:            account.GetAddress(),
+			RefundAddress:         account.GetAddress(),
+			FailureHandleStrategy: 0,
+			CallbackData:          []byte{'1'},
+		}, nil)
 
 	tx, err := client.SendMessages(context.Background(), messages.Build())
 	if err != nil {
